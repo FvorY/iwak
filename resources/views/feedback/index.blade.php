0543a65aa181715733a0241294e1ac543b5b961a
BRIGHTON Programmer <brighton.programmer@gmail.com>
2021-09-24 19:31:13 +0700

    Composer require php

---
+++ resources/views/feedback/index.blade.php
@@ -0,0 +1,141 @@
+@extends('main')
+@section('content')
+
+<style type="text/css">
+
+</style>
+<!-- partial -->
+<div class="content-wrapper">
+  <div class="row">
+    <div class="col-lg-12">
+      <nav aria-label="breadcrumb" role="navigation">
+        <ol class="breadcrumb bg-info">
+          <li class="breadcrumb-item"><i class="fa fa-home"></i>&nbsp;<a href="{{url('/home')}}">Home</a></li>
+          {{-- <li class="breadcrumb-item">Setup Master Tagihan</li> --}}
+          <li class="breadcrumb-item active" aria-current="page">Feedback / Review</li>
+        </ol>
+      </nav>
+    </div>
+  	<div class="col-lg-12 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="card-title">Feedback / Review</h4>
+                    <div class="col-md-12 col-sm-12 col-xs-12" align="right" style="margin-bottom: 15px;">
+                      {{-- @if(Auth::user()->akses('MASTER DATA STATUS','tambah')) --}}
+                    	<button type="button" class="btn btn-info" data-toggle="modal" data-target="#tambah"><i class="fa fa-plus"></i>&nbsp;&nbsp;Add Data</button>
+                      {{-- @endif --}}
+                    </div>
+                    <div class="table-responsive">
+        				        <table class="table table_status table-hover " id="table-data" cellspacing="0">
+                            <thead class="bg-gradient-info">
+                              <tr>
+                                <th>No</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                                <th>Message</th>
+                                <th>Action</th>
+                              </tr>
+                            </thead>
+
+                            <tbody>
+
+                            </tbody>
+
+
+                        </table>
+                    </div>
+                  </div>
+                </div>
+    </div>
+  </div>
+</div>
+<!-- content-wrapper ends -->
+@endsection
+@section('extra_script')
+<script>
+
+var table = $('#table-data').DataTable({
+        processing: true,
+        // responsive:true,
+        serverSide: true,
+        searching: true,
+        paging: true,
+        dom: 'Bfrtip',
+        title: '',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ],
+        ajax: {
+            url:'{{ url('/contacttable') }}',
+        },
+        columnDefs: [
+
+              {
+                 targets: 0 ,
+                 className: 'center id'
+              },
+              {
+                 targets: 1,
+                 className: 'center'
+              },
+              {
+                 targets: 2,
+                 className: 'center'
+              },
+              {
+                 targets: 3,
+                 className: 'center'
+              },
+              {
+                 targets: 4,
+                 className: 'center'
+              }
+            ],
+        "columns": [
+          {data: 'DT_Row_Index', name: 'DT_Row_Index'},
+          {data: 'email', name: 'email'},
+          {data: 'phone', name: 'phone'},
+          {data: 'message', name: 'message'},
+          {data: 'aksi', name: 'aksi'},
+
+        ]
+  });
+
+
+  function hapus(id) {
+    iziToast.question({
+      close: false,
+  		overlay: true,
+  		displayMode: 'once',
+  		title: 'Hapus data',
+  		message: 'Apakah anda yakin ?',
+  		position: 'center',
+  		buttons: [
+  			['<button><b>Ya</b></button>', function (instance, toast) {
+          $.ajax({
+            url:baseUrl + '/hapuscontact',
+            data:{id},
+            dataType:'json',
+            success:function(data){
+              iziToast.success({
+                  icon: 'fa fa-trash',
+                  message: 'Data Berhasil Dihapus!',
+              });
+
+              reloadall();
+            }
+          });
+  			}, true],
+  			['<button>Tidak</button>', function (instance, toast) {
+  				instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+  			}],
+  		]
+  	});
+  }
+
+  function reloadall() {
+    table.ajax.reload();
+  }
+
+</script>
+@endsection
