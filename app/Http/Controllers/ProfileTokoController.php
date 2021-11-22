88b81c82cc97b52352ddcb493035d9949c7aca90
BRIGHTON Programmer <brighton.programmer@gmail.com>
2021-11-22 10:22:07 +0700

    API done

--- app/Http/Controllers/ProfileTokoController.php
+++ app/Http/Controllers/ProfileTokoController.php
@@ -25,8 +25,55 @@ use Response;
 class ProfileTokoController extends Controller
 {
     public function index($id) {
-      if (Auth::user()->id_account == $id) {
-        return view('error-404');
+      if (Auth::check()) {
+        if (Auth::user()->id_account == $id) {
+          return view('error-404');
+        } else {
+          $sort = "DESC";
+          $sortfield = "name";
+
+          $cek = DB::table("account")
+                  ->where("id_account", $id)
+                  ->first();
+
+          $produk = DB::table("produk")
+                    ->join('imageproduk', 'imageproduk.id_produk', '=', 'produk.id_produk')
+                    ->join("account", 'produk.id_account', 'account.id_account')
+                    ->where("produk.id_account", $id)
+                    ->groupby("imageproduk.id_produk")
+                    ->orderby('produk.'.$sortfield, $sort)
+                    ->paginate(10);
+                    // dd($produk);
+          if (substr($cek->phone, 0, 1) == "+") {
+            $cek->phone = str_replace("+", "", $cek->phone);
+          } else if (substr($cek->phone, 0, 1) == "0") {
+            $cek->phone = str_replace("0", "62", $cek->phone);
+          }
+
+          if ($cek == null) {
+              return redirect('/');
+          } else {
+            if ($cek->namatoko == null || $cek->istoko == "N") {
+              return redirect('/');
+            } else {
+              $countproduk = DB::table("produk")
+                            ->where("produk.stock", '>' , 0)
+                            ->orderby('produk.sold', 'DESC')
+                            ->where("id_account", $id)
+                            ->count();
+
+              $countreview = DB::table("feedback")
+                            ->where("id_toko", $id)
+                            ->count();
+
+              $avgstar = DB::table("feedback")
+                            ->where("id_toko", $id)
+                            ->avg('star');
+
+              return view('toko_profile', compact('cek', 'countproduk', 'countreview', 'avgstar','produk'));
+            }
+          }
+        }
       } else {
         $sort = "DESC";
         $sortfield = "name";
@@ -73,6 +120,7 @@ class ProfileTokoController extends Controller
           }
         }
       }
+
     }
 
     public function apitoko(Request $req) {
