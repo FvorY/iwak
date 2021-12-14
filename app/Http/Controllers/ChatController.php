c7e3ddce29e86a0d281c69a9f1a92441956c10c0
BRIGHTON Programmer <brighton.programmer@gmail.com>
2021-12-14 08:10:08 +0700

    Fixing chat api

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -24,9 +24,9 @@ use Response;
 
 class ChatController extends Controller
 {
-    public function __construct(){
-        $this->middleware('auth');
-    }
+    // public function __construct(){
+    //     $this->middleware('auth');
+    // }
 
     public function index() {
       return view('chat');
